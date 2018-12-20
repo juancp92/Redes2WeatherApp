7a49703946d043d21849107c5e0c97ea6fee2b2b
juancp92 <31323527+juancp92@users.noreply.github.com>
2018-12-20 10:27:50 -0300

    Update weather.service.ts

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -6,16 +6,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class WeatherService {
-
+  
+  //URL of the Firebase Function to get the weather forecast
   readonly ROOT_URL = 'https://us-central1-net2weatherapp.cloudfunctions.net/darkSkyProxy';
 
   constructor(private http: HttpClient) { }
-
+  
+  //Function to pass the parameters and get the forecast
   currentForecast(lat: number, lng: number): Observable<any> {
     let params = new HttpParams();
     params = params.set('lat', lat.toString());
     params = params.set('lng', lng.toString());
-    console.log(this.ROOT_URL, params);
     return this.http.get(this.ROOT_URL, { params });
   }
 
